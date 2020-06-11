876b17ace0b0573f4dade6c2205c41ef36d1fd4b
Mrudul Katti <mrudulkatti@yahoo.com>
2020-06-11 20:53:23 +0530

    CustomDirectives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,6 +52,7 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     ContactComponent,
     LoginComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
